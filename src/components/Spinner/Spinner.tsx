33e5970c0f686f731b812e972094953cbc76bdea
Ayechanpaing31 <116068529+Ayechanpaing31@users.noreply.github.com>
2024-04-24 00:39:12 +0800

    feat: add popconfirm and spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { FC } from 'react'
+import { Loader2, LucideProps } from 'lucide-react'
+import { cn } from '../../lib/utils'
+import "../styles.css";
+
+interface SpinnerProps extends LucideProps {}
+
+const Spinner: FC<SpinnerProps> = ({ className, ...props }) => {
+  return (
+    
+    <div className={cn('mr-2 h-12 w-12 relative', className)}>
+    <div className={cn('absolute inset-0 flex justify-center items-center animate-spin')}>
+      <Loader2 {...props} />
+    </div>
+  </div>
+   
+  )
+}
+
+export default Spinner;
