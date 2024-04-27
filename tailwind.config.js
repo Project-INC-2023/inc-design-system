1f290e8329b4ca8f3c6d3b1e6eb8c565b30524f7
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-04-27 20:59:49 +0800

    chore: cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,23 +43,6 @@ module.exports = {
 
         // Background colors
         background: "#FDFCFF",
-        
-      },
-
-      // Animation for accordion component
-      keyframes: {
-        slideDown: {
-          from: { height: '0px' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        slideUp: {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0px' },
-        },
-      },
-      animation: {
-        slideDown: 'slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)',
-        slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)',
       },
     },
   },
