eca82d0953b0d5e848fac2935e39e3b2bb5cd922
4rchx824 <archielaw824@gmail.com>
2024-04-27 18:13:03 +0800

    fix: some typos and react import

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import * as React from "react";
-
+import React from "react";
 import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
@@ -21,7 +20,6 @@ const inputVariants = cva(
     },
   }
 );
-
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof inputVariants> {
