35945eee9f572cc63de40d8c5a84973c5809d70f
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-05-02 14:25:10 +0800

    laptop

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,33 @@
+import React, {
+  ReactElement,
+  createContext,
+  forwardRef,
+  useContext,
+  useState,
+} from "react";
 
-import React from 'react'
+export type ModalProps = {
+  children: React.ReactNode;
+};
 
-type Props = {}
+type ModalContextType = {
+  isVisible: boolean;
+  toggleVisibility: () => void;
+};
 
-const Modal = (props: Props) => {
-  return (
-    <div>Modal</div>
-  )
-}
+const ModalContext = createContext({
+  isVisible: false,
+  toggleVisibility: () => {},
+} as ModalContextType);
 
-export default Modal
+export const useModal = () => useContext(ModalContext);
+
+const Modal = React.forwardRef<ReactElement, ModalProps>(
+  ({ children, ...props }, forwardRef) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    return <div>Modal</div>; // Add a return statement here
+  }
+); // Add a semicolon here
+
+export default Modal;
