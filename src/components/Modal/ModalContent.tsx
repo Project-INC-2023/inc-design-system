a087a3edb0233c542e48d9d2373594fae15c13d7
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-05-04 02:59:07 +0800

    style: Update ModalContent styling for better responsiveness

--- src/components/Modal/ModalContent.tsx
+++ src/components/Modal/ModalContent.tsx
@@ -43,7 +43,7 @@ const ModalContent = forwardRef<HTMLDivElement, ModalContentProps>(
           {...props}
           ref={forwardedRef}
           className={cn(
-            "data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[700px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none",
+            "data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[80vw] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-6 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none",
             className
           )}
         >
