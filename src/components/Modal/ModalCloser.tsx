812fac2dea62a87ae5a4d5bf69d98e0091814198
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-05-04 01:54:03 +0800

    style: closer accepts classname and styling for content fixed

--- src/components/Modal/ModalCloser.tsx
+++ src/components/Modal/ModalCloser.tsx
@@ -1,14 +1,15 @@
 import React, { Children } from 'react'
 import {Close, DialogCloseProps} from '@radix-ui/react-dialog'
+import { cn } from '@/lib/utils'
 
 export interface ModalCloserProps extends DialogCloseProps{
   children: React.ReactNode
   className?: string
 }
 
-const ModalCloser = ({children, ...props}: ModalCloserProps) => {
+const ModalCloser = ({children, className, ...props}: ModalCloserProps) => {
   return (
-    <Close asChild {...props}>
+    <Close asChild {...props} className={cn(className)}>
       {children}
     </Close>
   )
