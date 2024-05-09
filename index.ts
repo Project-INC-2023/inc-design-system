2268f7f9616675d2087b5e29f7200125f8e1aedc
4rchx824 <archielaw824@gmail.com>
2024-05-10 01:32:59 +0800

    fix: moved animations to extend so it doesnt overwrite tailwind default animations

--- index.ts
+++ index.ts
@@ -35,7 +35,6 @@ export default plugin(
     addUtilities(newUtilities);
   },
   {
-    // content: ["./node_modules/@4rchx824/ui/dist/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {
         colors: {
@@ -80,34 +79,34 @@ export default plugin(
           background: "#FDFCFF",
           // Animation for Accordion
         },
-      },
 
-      keyframes: {
-        slideDown: {
-          from: { height: "0px" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        slideUp: {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0px" },
-        },
-        overlayShow: {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
-        },
-        contentShow: {
-          from: {
-            opacity: "0",
-            transform: "translate(-50%, -48%) scale(0.96)",
+        keyframes: {
+          slideDown: {
+            from: { height: "0px" },
+            to: { height: "var(--radix-accordion-content-height)" },
           },
-          to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+          slideUp: {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: "0px" },
+          },
+          overlayShow: {
+            from: { opacity: "0" },
+            to: { opacity: "1" },
+          },
+          contentShow: {
+            from: {
+              opacity: "0",
+              transform: "translate(-50%, -48%) scale(0.96)",
+            },
+            to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+          },
         },
-      },
-      animation: {
-        slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
-        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
-        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        animation: {
+          slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+          slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+          overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+          contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        },
       },
     },
   }
