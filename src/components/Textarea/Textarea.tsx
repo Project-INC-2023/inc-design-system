11743d6d1013b925df787151f8af035f772f459b
4rchx824 <archielaw824@gmail.com>
2024-05-18 23:37:44 +0800

    fix: textarea and sheet

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -35,24 +35,39 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     };
 
     return (
-      <div className={cn(["flex flex-col", containerClassName])}>
-        <textarea
-          onChange={handleChange}
-          className={cn(
-            "min-h-[60px] text-sm p-2 flex w-full text-text-default rounded-lg bg-white border border-grey-300 focus-visible:border-primary-active  placeholder:text-grey-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-grey-100",
-            autosize && "resize-none",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-        {showCharCount && (
-          <h1 className="text-grey-400 self-end text-sm">
-            {text.length}
-            {props.maxLength && ` / ${props.maxLength}`}
-          </h1>
+      <>
+        {containerClassName && showCharCount ? (
+          <div className={cn(["flex flex-col", containerClassName])}>
+            <textarea
+              onChange={handleChange}
+              className={cn(
+                "min-h-[60px] text-sm p-2 flex w-full text-text-default rounded-lg bg-white border border-grey-300 focus-visible:border-primary-active  placeholder:text-grey-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-grey-100",
+                autosize && "resize-none",
+                className
+              )}
+              ref={ref}
+              {...props}
+            />
+            {showCharCount && (
+              <h1 className="text-grey-400 self-end text-sm">
+                {text.length}
+                {props.maxLength && ` / ${props.maxLength}`}
+              </h1>
+            )}
+          </div>
+        ) : (
+          <textarea
+            onChange={handleChange}
+            className={cn(
+              "min-h-[60px] text-sm p-2 flex w-full text-text-default rounded-lg bg-white border border-grey-300 focus-visible:border-primary-active  placeholder:text-grey-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-grey-100",
+              autosize && "resize-none",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
         )}
-      </div>
+      </>
     );
   }
 );
