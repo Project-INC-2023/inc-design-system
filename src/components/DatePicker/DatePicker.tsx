3ed2642de318b863ef254a3c85183e6d42593a8d
Zolastic <honhattienlul@gmail.com>
2024-07-30 20:10:48 +0800

    refactor: create BaseDatePickerProps interface to reduce redundancy
    - Extract common props into BaseDatePickerProps interface
    - Extend BaseDatePickerProps in DatePickerProps and RangeDatePickerProps
    - Simplify component prop definitions

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -15,12 +15,71 @@ import {
 } from "../Popover/Popover";
 import { type Matcher } from "react-day-picker";
 
-interface DatePickerProps extends PopoverProps {
+interface BaseDatePickerProps extends PopoverProps {
   /**
    * The current date.
    * @default new Date()
    */
   today?: Date;
+  /**
+   * A boolean that determines whether the date picker is disabled. If `true`, the date picker is disabled and cannot be interacted with. If `false` or omitted, the date picker is enabled.
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * A string that is added to the class list of the button element in the date picker. This can be used to add custom styles to the button.
+   * @default ""
+   */
+  buttonClassName?: string;
+  /**
+   * The `disabledDates` prop allows you to specify which days should be disabled in the calendar.
+   * You can disable days in various ways:
+   *
+   * - Disable specific dates:
+   *   @example disabledDates={[new Date(2022, 0, 1), new Date(2022, 0, 2)]} // Disable January 1st and 2nd, 2022
+   *   @example disabledDates={new Date(2022, 0, 1)} // Disable January 1st, 2022
+   *   @example disabledDates={[]} // Disable no dates
+   *   @example disabledDates={undefined} // Disable no dates
+   *
+   * - Disable dates before and/or after specific dates:
+   *   @example
+   *   disabledDates={{
+   *     before: new Date("2024-4-1"),
+   *     after: new Date("2024-4-15"),
+   *   }} // Disable dates before April 1st, 2024 and after April 15th, 2024
+   *  @example
+   *  disabledDates={{
+   *     before: new Date()
+   *   }} // Disable dates before today
+   *  @example
+   *  disabledDates={{
+   *     after: new Date()
+   *   }} // Disable dates after today
+   *
+   * - Disable specific days of the week (e.g., Sundays and Saturdays):
+   *   @example
+   *   disabledDates={{
+   *     daysOfWeek: [0, 6],
+   *   }} // Disable Sundays and Saturdays
+   *
+   * - Combine disabling specific days of the week and specific dates:
+   *   @example
+   *   disabledDates={{
+   *     daysOfWeek: [0, 6],
+   *     dates: [new Date("2024-4-30"), new Date("2024-4-23")],
+   *   }} // Disable Sundays, Saturdays, April 30th, 2024, and April 23rd, 2024
+   *
+   * - Disable a range of dates:
+   *   @example
+   *   disabledDates={{
+   *     from: new Date("2024-4-1"),
+   *     to: new Date("2024-4-15"),
+   *   }} // Disable dates from April 1st, 2024 to April 15th, 2024
+   */
+  disabledDates?: Matcher | Matcher[] | undefined;
+}
+
+interface DatePickerProps extends BaseDatePickerProps {
   /**
    * The currently selected date. If a date is selected, it will be displayed in the date picker. If no date is selected, the placeholder text will be displayed instead.
    */
@@ -34,70 +93,9 @@ interface DatePickerProps extends PopoverProps {
    * @default "Pick a date"
    */
   placeholder?: string;
-  /**
-   * A boolean that determines whether the date picker is disabled. If `true`, the date picker is disabled and cannot be interacted with. If `false` or omitted, the date picker is enabled.
-   * @default false
-   */
-  disabled?: boolean;
-  /**
-   * A string that is added to the class list of the button element in the date picker. This can be used to add custom styles to the button.
-   * @default ""
-   */
-  buttonClassName?: string;
-  /**
-   * The `disabledDays` prop allows you to specify which days should be disabled in the calendar.
-   * You can disable days in various ways:
-   *
-   * - Disable specific dates:
-   *   @example disabledDays={[new Date(2022, 0, 1), new Date(2022, 0, 2)]} // Disable January 1st and 2nd, 2022
-   *   @example disabledDays={new Date(2022, 0, 1)} // Disable January 1st, 2022
-   *   @example disabledDays={[]} // Disable no dates
-   *   @example disabledDays={undefined} // Disable no dates
-   *
-   * - Disable dates before and/or after specific dates:
-   *   @example
-   *   disabled={{
-   *     before: new Date("2024-4-1"),
-   *     after: new Date("2024-4-15"),
-   *   }} // Disable dates before April 1st, 2024 and after April 15th, 2024
-   *  @example
-   *  disabled={{
-   *     before: new Date()
-   *   }} // Disable dates before today
-   *  @example
-   *  disabled={{
-   *     after: new Date()
-   *   }} // Disable dates after today
-   *
-   * - Disable specific days of the week (e.g., Sundays and Saturdays):
-   *   @example
-   *   disabled={{
-   *     daysOfWeek: [0, 6],
-   *   }} // Disable Sundays and Saturdays
-   *
-   * - Combine disabling specific days of the week and specific dates:
-   *   @example
-   *   disabled={{
-   *     daysOfWeek: [0, 6],
-   *     dates: [new Date("2024-4-30"), new Date("2024-4-23")],
-   *   }} // Disable Sundays, Saturdays, April 30th, 2024, and April 23rd, 2024
-   *
-   * - Disable a range of dates:
-   *   @example
-   *   disabled={{
-   *     from: new Date("2024-4-1"),
-   *     to: new Date("2024-4-15"),
-   *   }} // Disable dates from April 1st, 2024 to April 15th, 2024
-   */
-  disabledDays?: Matcher | Matcher[] | undefined;
 }
 
-interface RangeDatePickerProps extends PopoverProps {
-  /**
-   * The current date.
-   * @default new Date()
-   */
-  today?: Date;
+interface RangeDatePickerProps extends BaseDatePickerProps {
   /**
    * The currently selected date. If a date is selected, it will be displayed in the date picker. If no date is selected, the placeholder text will be displayed instead.
    */
@@ -106,16 +104,6 @@ interface RangeDatePickerProps extends PopoverProps {
    * A callback function that is called when a date is selected in the date picker. The selected date is passed as an argument to this function.
    */
   onSelect?: (date: CalendarDateRange) => void;
-  /**
-   * A boolean that determines whether the date picker is disabled. If `true`, the date picker is disabled and cannot be interacted with. If `false` or omitted, the date picker is enabled.
-   * @default false
-   */
-  disabled?: boolean;
-  /**
-   * A string that is added to the class list of the button element in the date picker. This can be used to add custom styles to the button.
-   * @default ""
-   */
-  buttonClassName?: string;
   /**
    * The placeholder text that is displayed when the start date is not selected.
    * @default "Start date"
@@ -126,44 +114,6 @@ interface RangeDatePickerProps extends PopoverProps {
    * @default "End date"
    */
   endDatePlaceholder?: string;
-  /**
-   * The `disabledDays` prop allows you to specify which days should be disabled in the calendar.
-   * You can disable days in various ways:
-   *
-   * - Disable specific dates:
-   *   @example disabledDays={[new Date(2022, 0, 1), new Date(2022, 0, 2)]} // Disable January 1st and 2nd, 2022
-   *   @example disabledDays={new Date(2022, 0, 1)} // Disable January 1st, 2022
-   *   @example disabledDays={[]} // Disable no dates
-   *   @example disabledDays={undefined} // Disable no dates
-   *
-   * - Disable dates before and/or after specific dates:
-   *   @example
-   *   disabled={{
-   *     before: new Date("2024-4-1"),
-   *     after: new Date("2024-4-15"),
-   *   }} // Disable dates before April 1st, 2024 and after April 15th, 2024
-   *
-   * - Disable specific days of the week (e.g., Sundays and Saturdays):
-   *   @example
-   *   disabled={{
-   *     daysOfWeek: [0, 6],
-   *   }} // Disable Sundays and Saturdays
-   *
-   * - Combine disabling specific days of the week and specific dates:
-   *   @example
-   *   disabled={{
-   *     daysOfWeek: [0, 6],
-   *     dates: [new Date("2024-4-30"), new Date("2024-4-23")],
-   *   }} // Disable Sundays, Saturdays, April 30th, 2024, and April 23rd, 2024
-   *
-   * - Disable a range of dates:
-   *   @example
-   *   disabled={{
-   *     from: new Date("2024-4-1"),
-   *     to: new Date("2024-4-15"),
-   *   }} // Disable dates from April 1st, 2024 to April 15th, 2024
-   */
-  disabledDays?: Matcher | Matcher[] | undefined;
 }
 
 const DatePicker = ({
@@ -173,7 +123,7 @@ const DatePicker = ({
   placeholder = "Pick a date",
   disabled = false,
   buttonClassName,
-  disabledDays,
+  disabledDates,
   ...props
 }: DatePickerProps) => {
   const [date, setDate] = React.useState<Date | undefined>(selected);
@@ -207,7 +157,7 @@ const DatePicker = ({
           mode="single"
           selected={date}
           onSelect={handleOnSelect}
-          disabled={disabledDays}
+          disabled={disabledDates}
           initialFocus
         />
       </PopoverContent>
@@ -223,6 +173,7 @@ const RangeDatePicker = ({
   buttonClassName,
   startDatePlaceholder = "Start date",
   endDatePlaceholder = "End date",
+  disabledDates,
   ...props
 }: RangeDatePickerProps) => {
   const [date, setDate] = React.useState<CalendarDateRange | undefined>(
@@ -267,6 +218,7 @@ const RangeDatePicker = ({
           mode="range"
           selected={date}
           onSelect={handleOnSelect}
+          disabled={disabledDates}
           initialFocus
         />
       </PopoverContent>
